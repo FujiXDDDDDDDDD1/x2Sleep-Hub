f103c6364757ce1f1e48ab341e62cb5c149a0750
FujiXDDDDDDDDD1 <71167709+FujiXDDDDDDDDD1@users.noreply.github.com>
2023-05-27 22:43:44 +0700

    Update ls.lua

--- ls.lua
+++ ls.lua
@@ -1,5 +1,5 @@
 getgenv().Games = {
-    [6186867282] = "https://raw.githubusercontent.com/FujiXDDDDDDDDD1/x2Sleep-Hub/main/ps.lua",
+    [5956785391] = "https://raw.githubusercontent.com/FujiXDDDDDDDDD1/x2Sleep-Hub/main/ps.lua",
 }
 
 getgenv().Unknown = {}
